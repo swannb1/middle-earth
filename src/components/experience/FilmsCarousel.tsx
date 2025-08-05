73cda49717e0b47ed8d49c8852564b763eaf8738
Brandon Swann <brandonswann@Brandons-MacBook-Air.local>
2025-08-05 11:58:14 -0600

    Add Films

--- src/components/experience/FilmsCarousel.tsx
+++ src/components/experience/FilmsCarousel.tsx
@@ -20,6 +20,21 @@ function FilmsCarousel() {
       <Carousel.Item>
         <ExperienceCarouselItem name="Return of the King" id="zckJCxYxn1g?si=dbIgZ4aDpH0V2FWa" />
       </Carousel.Item>
+      <Carousel.Item>
+        <ExperienceCarouselItem name="War of the Rohirrim" id="gCUg6Td5fgQ?si=PMeWXH-Df_7_hzW-" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <ExperienceCarouselItem name="The Hobbit Animated" id="WqGzCOL3XU4?si=E1Z8Uq9_hXeDSJBm" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <ExperienceCarouselItem name="An Unexpected Journey" id="SDnYMbYB-nU?si=_YpUs1-HUAXIbf_4" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <ExperienceCarouselItem name="The Desolation of Smaug" id="Os1G8RtqY2c?si=5A2IK13lASWDWDXZ" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <ExperienceCarouselItem name="The Battle of the Five Armies" id="iVAgTiBrrDA?si=EidVEdbAqoD_tmd5" />
+      </Carousel.Item>
     </Carousel>
   );
 }
